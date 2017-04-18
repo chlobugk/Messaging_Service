48296b848444ac7dd1c3a5b49aa0a5f8a05b7df9
Chloe Krainbucher <chloekrainbucher@poiuytrohb8ubh8.home>
2017-04-18 10:53:32 -0400

    start to setup

--- app.rb
+++ app.rb
@@ -1,1 +1,13 @@
-#hello
+require 'sinatra'
+require 'pg'
+load './local_env.rb' if File.exists?('./local_env.rb')
+db_params = {
+    host: ENV['host'],
+    port: ENV['port'],
+    dbname: ENV['db_name'],
+    user: ENV['user'],
+    password: ENV['password']
+}
+
+db = PG::Connection.new(db_params)
+
