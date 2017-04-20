7fa8cf7626064de3e9feb619294473f6395c29ad
Chloe Krainbucher <chloekrainbucher@pOiuytr-easd-bh78oohb8ubh8.local>
2017-04-20 18:45:06 -0400

    only creates account with valid inputs, added css

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'pg'
+require_relative 'functions.rb'
 load './local_env.rb' if File.exists?('./local_env.rb')
 db_params = {
     host: ENV['host'],
@@ -17,22 +18,41 @@ get '/' do
 end
 
 post '/login' do
+	redirect '/login'
+end
+
+get '/login' do
 	erb :login
 end
 
 post '/create_account' do
-	erb :create_account
+	redirect '/create_account'
 end
 
+get '/create_account' do
+	message1 = nil
+	message2 = nil
+	erb :create_account, locals: {message1: message1, message2: message2}
+end
+
+get '/invalid_credentials' do
+	message1 = 'One or more of your credentials was invalid.'
+	message2 = 'Please make sure your password is at least 6 characters.'
+	erb :create_account, locals: {message1: message1, message2: message2}
+end
 
 post '/created' do
-		accounts=db.exec("SELECT full_name, username, password FROM accounts");
+	if valid_credentials?(params[:full_name], params[:username], params[:password]) == false	
+		redirect '/invalid_credentials'
+	else
 		#this post adds created account info to database
+		accounts=db.exec("SELECT full_name, username, password FROM accounts");
 		full_name = params[:full_name]
 		username = params[:username]
 		password = params[:password] 
 		db.exec("INSERT INTO accounts(full_name, username, password) VALUES('#{full_name}', '#{username}', '#{password}')")
 		erb :message, locals: {username: username}
+	end
 end
 
 post '/message' do
--- functions.rb
+++ functions.rb
@@ -1,4 +1,15 @@
 def valid_password?(password)
-	password.length > 5
+	password.to_s.length > 5
 end
 
+def valid_username?(username)
+	username.to_s.length > 0
+end
+
+def valid_full_name?(full_name)
+	full_name.to_s.include?(' ') && full_name.to_s.length > 1
+end
+
+def valid_credentials?(full_name, username, password)
+	valid_full_name?(full_name) && valid_username?(username) && valid_password?(password)
+end
