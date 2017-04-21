b8ebf8139ce10fd69198684bbaddf928e3b2af0a
Daniel Parks <dparks715@yahoo.com>
2017-04-21 09:15:46 -0400

    Added function to prevent repeat usernames

--- functions.rb
+++ functions.rb
@@ -1,3 +1,6 @@
+require 'pg'
+load './local_env.rb' if File.exists?('./local_env.rb')
+
 def valid_password?(password)
 	password.to_s.length > 5
 end
@@ -12,4 +15,25 @@ end
 
 def valid_credentials?(full_name, username, password)
 	valid_full_name?(full_name) && valid_username?(username) && valid_password?(password)
+end
+
+def username_not_unique?(new_username)
+	db_params = {
+    host: ENV['host'],
+    port: ENV['port'],
+    dbname: ENV['db_name'],
+    user: ENV['user'],
+    password: ENV['password']
+	}
+
+	db = PG::Connection.new(db_params)
+	
+	dbname=db.exec("SELECT username FROM accounts")
+	results = false
+	dbname.each do |item|
+		if item['username'] == new_username
+			results = true
+		end
+	end
+	results
 end
