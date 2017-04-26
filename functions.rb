b423c07f2b810510a426ba5c33c6dcf80266389d
TMerrick55 <tmerrick44@gmail.com>
2017-04-26 14:23:16 -0400

    Removal of unneeded text from files as a result of last push

--- functions.rb
+++ functions.rb
@@ -4,11 +4,7 @@ load './local_env.rb' if File.exists?('./local_env.rb')
 def valid_password?(password)
 	password.to_s.length > 5
 end
-<<<<<<< HEAD
  
-=======
-
->>>>>>> ff8d3085d425a3328687b7aeeb3d4ee273b3bc54
 def valid_username?(username)
 	username.to_s.length > 0
 end
@@ -31,11 +27,7 @@ def username_not_unique?(new_username)
 	}
 
 	db = PG::Connection.new(db_params)
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> ff8d3085d425a3328687b7aeeb3d4ee273b3bc54
 	dbname=db.exec("SELECT username FROM accounts")
 	results = false
 	dbname.each do |item|
@@ -48,7 +40,6 @@ end
 
 def login_match?(log_username, log_password)
 	db_params = {
-<<<<<<< HEAD
     host: ENV['host'], 
     port: ENV['port'], 
     dbname: ENV['db_name'],
@@ -57,18 +48,7 @@ def login_match?(log_username, log_password)
 	}
 
 	db = PG::Connection.new(db_params)
-
-=======
-    host: ENV['host'],
-    port: ENV['port'],
-    dbname: ENV['db_name'],
-    user: ENV['user'],
-    password: ENV['password']
-	}
-
-	db = PG::Connection.new(db_params)
 	
->>>>>>> ff8d3085d425a3328687b7aeeb3d4ee273b3bc54
 	dbname=db.exec("SELECT username, password FROM accounts")
 	results = false
 	dbname.each do |item|
@@ -89,34 +69,15 @@ def new_message?(username, friendname)
 	}
 
 	db = PG::Connection.new(db_params)
-<<<<<<< HEAD
+
 	dbname=db.exec("SELECT user, friend FROM messages") 
 	results = false
 	if username_not_unique?(friendname) == true
 		dbname.each do |item|
 			if item['from'] == username && item['to'] == friendname 
-=======
-	dbname=db.exec("SELECT user, friend FROM messages")
-	results = false
-	if username_not_unique?(friendname) == true
-		dbname.each do |item|
-			if item['from'] == username && item['to'] == friendname
->>>>>>> ff8d3085d425a3328687b7aeeb3d4ee273b3bc54
 				results = true
 			end
 		end
 	end
 	results
-<<<<<<< HEAD
 end
-=======
-end
-
-
-
-
-
-
-
-
->>>>>>> ff8d3085d425a3328687b7aeeb3d4ee273b3bc54
